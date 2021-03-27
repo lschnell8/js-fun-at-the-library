609195d6183b5b8e759a8071fc3ec18f507c78c3
Lindsay Schnell <lschnell8@gmail.com>
2021-03-27 13:51:58 -0600

    Update shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,45 @@
+function shelfBook(book, shelf) {
+  if(shelf.length < 3) {
+  shelf.unshift(book)
+}
+  return shelf
+};
+
+
+function unshelfBook(bookTitle, shelf) {
+for(var i = 0; i < shelf.length; i++) {
+  if(bookTitle === shelf[i].title) {
+    return shelf.splice([i], 1)
+  }
+}
+};
+
+function listTitles(shelf) {
+var list = "";
+for(var i = 0; i < shelf.length; i++) {
+  if([i] < 2) {
+    list += shelf[i].title + ", "
+  }else {
+    list += shelf[i].title
+  }
+}
+return list
+};
+
+function searchShelf(shelf, title) {
+for(i = 0; i < shelf.length; i++) {
+  if(shelf[i].title === title) {
+    return true
+}
+}
+  return false
+};
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
