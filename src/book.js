e83d85d984c59ff5d508a86a79c45fa2c02f9478
Lindsay Schnell <lschnell8@gmail.com>
2021-03-26 21:06:56 -0600

    initial commit

--- src/book.js
+++ src/book.js
@@ -1,12 +1,40 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`
+};
 
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name, age, pronouns};
+};
+function saveReview(content, reviews) {
+if (reviews.includes(content)) {
+  return 'Thanks for your submission'
+}else {
+  return reviews.push(content);
+}
+};
+
+function calculatePageCount(pages) {
+  return pages.length * 20
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+return book = {
+  title : bookTitle,
+  mainCharacter : bookCharacter,
+  pageCount : bookTitle.length * 20,
+  genre : genre}
+};
+
+function editBook(book) {
+book.pageCount *= .75
+return book.pageCount
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/warm-up.js
+++ src/warm-up.js
@@ -12,27 +12,28 @@
 
 // var title = "Harry Potter";
 // var ...
-
+var title = 'Brave New World'
+var pages = 312
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
-
+// of what it currently is
+pages = 312 / 4
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
+var sequel = title + ' Revisited'
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
-
+isFavorite = true
 
 
 
@@ -41,22 +42,30 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
-
+isShortStory = pages < 25
 
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
-
+if(pages < 1000) {
+  console.log('I could read that in a day!')
+} else {
+  console.log('Ok, maybe I could read that in a day and a half')
+}
 
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
-
+if(isFavorite = true) {
+  console.log('My favorite book is ' + title)
+} else {
+  console.log('I recently read the ' + title + ' book')
+}
 
 
 //------------------------- Arrays -------------------------//
@@ -64,25 +73,29 @@
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
-
+var books = ['Ghost', 'Go Ask Alice', 'The Foot Book', 'The Outsiders', 'As I Lay Dying']
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+books[2]
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
-
+var belowTwoHundred = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+belowTwoHundred[4]
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable "dictionary" and assign to it an object literal with two
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
-
+var dictionary = {
+  name: "Webster's",
+  hasThesaurus: false
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+dictionary.pageCount = 720
