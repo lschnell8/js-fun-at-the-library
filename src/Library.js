947e4172c2878f060ffc6236dcf5be63e9d778c6
Lindsay Schnell <lschnell8@gmail.com>
2021-03-27 19:32:26 -0600

    Update Library.js and Library-test.js

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,32 @@
+function createLibrary(library) {
+  return {
+    name: library,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+};
 
+function addBook(library, book) {
+  if(book.genre === 'fantasy') {
+return library.shelves.fantasy.push(book);
+} else {
+  return library.shelves.nonFiction.push(book);
+}
+};
 
+function checkoutBook(library, book, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if(library.shelves[genre][i].title === book) {
+library.shelves[genre].splice([i], 1)
+  return `You have now checked out ${book} from ${library}`
+}
+  }
+}
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
