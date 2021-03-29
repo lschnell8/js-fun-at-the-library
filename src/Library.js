b21ee525d56d487dd7a01c5f05725392766da7f9
Lindsay Schnell <lschnell8@gmail.com>
2021-03-28 20:50:17 -0600

    Update Library.js

--- src/Library.js
+++ src/Library.js
@@ -20,15 +20,15 @@ return library.shelves.fantasy.push(book);
 };
 
 function checkoutBook(library, book, genre) {
-  for (var i = 0; i <= library.shelves[genre].length; i++) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
     if(library.shelves[genre][i].title === book) {
 library.shelves[genre].splice([i], 1)
   return `You have now checked out ${book} from the ${library.name}`
-} else if(library.shelves[genre][i].title !== book) {
-  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
 }
 }
+return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
 }
+
 module.exports = {
   createLibrary,
   addBook,
